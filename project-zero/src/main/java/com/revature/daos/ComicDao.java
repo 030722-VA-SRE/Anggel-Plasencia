cbb8684f76b2ef503de7395b0405900489fec9ec
Anggel <anggelt.plasencia@gmail.com>
2022-03-22 15:53:02 -0400

    fixing issue

--- project-zero/src/main/java/com/revature/daos/ComicDao.java
+++ project-zero/src/main/java/com/revature/daos/ComicDao.java
@@ -5,9 +5,20 @@ import com.revature.models.Comics;
 
 public interface ComicDao {
 	
-	
+	//list all
 	public List<Comics> getAllComics();
-	public Comics getComicById(int id);
-	public Comics getComicByGenre(String genre);
+	
+	//by criteria
+	public Comics getById(int id);
+	public List<Comics> getByGenre(String genre);
+	public List<Comics> getByComicName(String comic);
+	public List<Comics> getByComicAndGenre(String comic, String genre);
+	
+	//creating, update, and delete 
+	public int addComic(Comics comic);
+	public boolean updateComic(Comics comic);
+	public boolean deleteComic(int id);
+
+	
 
 }
--- project-zero/src/main/java/com/revature/services/ComicServices.java
+++ project-zero/src/main/java/com/revature/services/ComicServices.java
@@ -1,11 +1,11 @@
 package com.revature.services;
+
 import java.util.List;
 
 import com.revature.daos.ComicDao;
 import com.revature.daos.ComicPostgres;
 import com.revature.exceptions.ItemNotFoundException;
 import com.revature.models.Comics;
-
 
 public class ComicServices {
 
@@ -14,39 +14,82 @@ public class ComicServices {
 	public ComicServices() {
 		
 		cd = new ComicPostgres();
-		
 	}
-	
-	
+
 	public List<Comics> getAllComics() {
-		
+
 		return cd.getAllComics();
-		
+
 	}
-	
-	
-	//gets the comic id, if the comic's id is not found; throws exception
+
+	// gets the comic id, if the comic's id is not found; throws exception
 	public Comics getById(int id) throws ItemNotFoundException {
-		Comics comics = cd.getComicById(id);
-		
-		if(comics == null) {
+		Comics comics = cd.getById(id);
+
+		if (comics == null) {
 			throw new ItemNotFoundException();
 		}
 		return comics;
-		
+
 	}
-	
+
+	public boolean addComic(Comics comic) throws ItemNotFoundException {
+
+		int generatedId = cd.addComic(comic);
+
+		if (generatedId != -1) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean updateComic(Comics comic) throws ItemNotFoundException {
+
+		boolean cUpdate = cd.updateComic(comic);
+
+		if (!cUpdate) {
+
+			throw new ItemNotFoundException();
+		}
+		return cUpdate;
+	}
+
+	public boolean deleteComic(int id) throws ItemNotFoundException {
+		
+		boolean cDelete = cd.deleteComic(id);
+
+		if (!cDelete) {
+
+			throw new ItemNotFoundException();
+		}
+		return cDelete;
+
+	}
+
 	//gets the comic genre, if the comic's genre is not found; throws exception
-		public Comics getByGenre(String genre) throws ItemNotFoundException {
-			Comics comics = cd.getComicByGenre(genre);
+		public List<Comics> getByGenre(String genre) throws ItemNotFoundException {
+			List<Comics> gen = cd.getByGenre(genre);
 			
-			if(comics == null) {
+			if(gen == null) {
 				throw new ItemNotFoundException();
 			}
-			return comics;
+			return gen;
 			
 		}
-	
-	
-	
-}
+		
+		public List<Comics> getByComicName(String comic) throws ItemNotFoundException {
+			List<Comics> com = cd.getByComicName(comic);
+			
+			if(com == null) {
+				throw new ItemNotFoundException();
+			}
+			return com;
+			
+		}
+		
+		public List<Comics> getByComicAndGenre(String comic, String genre){
+			
+			return cd.getByComicAndGenre(comic, genre);
+		}
+
+}// End of CS
